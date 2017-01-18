f6ecf44dd12c2ab09a606fec8573b403d0e35f0c
rapigan <rapigan@gmail.com>
2017-01-18 07:49:47 -0800

    initial commit attention model

---
+++ phase_1_implementing_networks/seq2seq/attention_encoder_decoder/model.py
@@ -0,0 +1,9 @@
+"""
+based on http://arxiv.org/abs/1412.7449
+https://github.com/suriyadeepan/easy_seq2seq/blob/master/seq2seq_model.py
+https://github.com/tensorflow/models/blob/master/tutorials/rnn/translate/seq2seq_model.py
+
+"""
+
+
+
