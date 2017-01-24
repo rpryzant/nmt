b362ed81ebcac12c9bbeda7f5a26ad043feaae38
rapigan <rapigan@gmail.com>
2017-01-24 07:54:16 -0800

    added resources

--- phase_1_implementing_networks/seq2seq/attention_encoder_decoder/model.py
+++ phase_1_implementing_networks/seq2seq/attention_encoder_decoder/model.py
@@ -1,7 +1,17 @@
 """
-based on http://arxiv.org/abs/1412.7449
-https://github.com/suriyadeepan/easy_seq2seq/blob/master/seq2seq_model.py
-https://github.com/tensorflow/models/blob/master/tutorials/rnn/translate/seq2seq_model.py
+based on http://arxiv.org/abs/1412.7449 and https://arxiv.org/abs/1508.04025 (mostly the latter)
+
+
+
+=== ATTENTION RESOURCES:
+
+http://distill.pub/2016/augmented-rnns/
+https://github.com/lmthang/thesis/blob/master/thesis.pdf
+  pg 54
+https://arxiv.org/abs/1508.04025
+http://www.wildml.com/2016/01/attention-and-memory-in-deep-learning-and-nlp/
+https://blog.heuritech.com/2016/01/20/attention-mechanism/
+https://indico.io/blog/sequence-modeling-neural-networks-part2-attention-models/
 
 """
 
