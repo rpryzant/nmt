b7841915604a8d77e0013f8d7e14dc1143042035
rapigan <rapigan@gmail.com>
2017-03-01 10:48:45 -0800

    constants

---
+++ phase_3_learning/msc/constants.py
@@ -0,0 +1,13 @@
+
+
+
+class Constants:
+    PAD     = "<pad>"
+    START   = "<s>"
+    END     = "</s>"
+    UNK     = "<unk>"
+
+    PAD_I   = 0
+    START_I = 1
+    END_I   = 2
+    UNK_I   = 3
