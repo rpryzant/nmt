875438675af445e056933e0bc69665695543f624
rpryzant <rapigan@gmail.com>
2017-03-10 14:15:18 -0800

    generate_splits refactor

--- phase_3_learning/data/generate_splits.py
+++ phase_3_learning/data/generate_splits.py
@@ -1,9 +1,13 @@
 """
 
+=== DESCRIPTION
+Given two corpus files that contain corresponding translations,
+  one per line, this script generates train/dev/test indices 
+
+=== USAGE
 python generate_splits.py [en file] [other file]
 
 """
-
 import sys; sys.path.append('../')    # sigh 
 from msc.utils import file_length
 import random
@@ -11,36 +15,34 @@ import random
 
 max_source_len = 50
 max_target_len = 50
-
+train_n = 110000
+train_test_n = train_n / 25
 
 corpus = sys.argv[1]
 
+
+# read in corpii (?)
 lx = [x.strip().split() for x in open(sys.argv[1]).read().split('\n')]
 ly = [y.strip().split() for y in open(sys.argv[2]).read().split('\n')]
 
 
-# minus 2 for start/end tokens
+# get all sentences that are short enough for consideration
+#    (minus 2 for start/end tokens)
 valid_indices = [i for i in range(len(lx)) if\
                         len(lx[i]) < max_source_len - 2 and\
                         len(ly[i]) < max_target_len - 2]
 
 
-
-
-
-
+# shuffle indices and select train/test/val splits
 random.shuffle(valid_indices)
 
-indices = valid_indices[:110000]
-N = len(indices)
-train_test_n = N / 25
-
-
+indices = valid_indices[:train_n]
 train = indices[:N - (train_test_n * 2)]
-val = indices[len(train): N - train_test_n]
-test = indices[len(train) + len(val):]
+val = indices[train_n: train_n - train_test_n]
+test = indices[train_n + len(val):]
 
 
+# write output
 f= open('train', 'w')
 f.write('\n'.join(str(x) for x in train))
 f.close()
